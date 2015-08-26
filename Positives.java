e0fb3793c276b4a99cc200357dc448f9278512a1
rcvillareal <rodney.villareal@gmail.com>
2015-08-25 23:05:23 -0700

    Create Positives.java

---
+++ Positives.java
@@ -0,0 +1,14 @@
+/**
+ * Rodney Villareal
+ * CS 356 Project 2
+ * Positives class to find positive tweets
+ * with methods to calculate the percent.
+ * Was not fully implemented.
+ */
+
+public class Positives {
+	private int total;
+	private int positive;
+	private double percent;
+	private List<String>
+}
