b6a9686e36757b41a8d3db7e06e18ab7b7c68352
rcvillareal <rodney.villareal@gmail.com>
2015-08-25 23:04:50 -0700

    Create Observer.java

---
+++ Observer.java
@@ -0,0 +1,10 @@
+/**
+ * Rodney Villareal
+ * CS 356 Project 2
+ * Observer interface to implement the
+ * observer pattern. User Class implements
+ * this.
+ */
+public interface Observer {
+	void update(String id);
+}
