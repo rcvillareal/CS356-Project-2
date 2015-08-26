3f4a043bf949339c0d04c42fb2c90afe002522da
rcvillareal <rodney.villareal@gmail.com>
2015-08-25 23:01:55 -0700

    Create GUIDriver.java

---
+++ GUIDriver.java
@@ -0,0 +1,12 @@
+/**
+ * Rodney Villareal
+ * CS 356 Project 2
+ * GUI Driver class that gets an instance of
+ * the AdminControlPanel.
+ */
+
+public class GUIDriver {	
+	public static void main(String[] args) {
+		AdminControlPanel acp = AdminControlPanel.getInstance();
+	}
+}
